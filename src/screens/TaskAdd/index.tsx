5c6a6e11144cf221260c335b43c65fab8d186801
datenprojetos <sistema@daten.com.br>
2024-10-06 00:53:04 -0300

    camera

--- src/screens/TaskAdd/index.tsx
+++ src/screens/TaskAdd/index.tsx
@@ -3,6 +3,7 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useContext, useState } from 'react';
 import { Alert, Keyboard, Pressable, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import { TaskContext } from '../../context/TaskContext';
 import { Task } from '../../model/task';
@@ -18,6 +19,41 @@ export function TaskAdd() {
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+
+    const openGalery = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+
+          console.log(result);
+
+    if (!result.canceled) {
+      console.log(result.assets[0].uri);
+    }
+    }
+
+    const openCamera = async () => {
+        const { status } = await ImagePicker.requestCameraPermissionsAsync();
+        if (status !== 'granted') {
+            alert('Sorry, we need camera permissions to make this work!');
+            return;
+        }
+
+        const result = await ImagePicker.launchCameraAsync({
+            base64: true,  // Enable base64 encoding
+            quality: 1,    // Image quality (1 is highest)
+          });
+
+          if (!result.canceled) {
+            const base64Image = result.assets[0].base64;
+            console.log(result.assets[0].uri);  // Display the image
+            console.log(base64Image);             // Save the image in AsyncStorage
+          }
+      
+    }
 
     function addTask() {
         const taskAdd = {
@@ -58,13 +94,13 @@ export function TaskAdd() {
             <View style={styles.containerCamera}>
                 <TouchableOpacity
                     style={styles.camera}
-                    onPress={() => Alert.alert("teste")}>
+                    onPress={openGalery}>
                     <Feather name="image" size={30} color="#000"></Feather>
                     <Text>Imagens</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.camera}
-                    onPress={() => Alert.alert("teste")}>
+                    onPress={openCamera}>
                     <Feather name="camera" size={30} color="#000"></Feather>
                     <Text>Câmera</Text>
                 </TouchableOpacity>
